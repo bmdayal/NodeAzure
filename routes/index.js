886a6e1d728130866321db316c2091c4d59cbfbe
bmdayal <bmdayal@hotmail.com>
2015-09-06 01:39:18 -0500

    Message customized

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Node on my first Azure website' });
 });
 
 module.exports = router;
